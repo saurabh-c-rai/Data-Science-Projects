f6c77f5108ba769294b55cd38810aaa57d81b8c8
Saurabh Rai <raisaurabh04@gmail.com>
2020-07-20 14:41:15 +0530

    directory changes

--- NLP & DeepLearning/DeepLearning/Keras/InformationExtractionAndNER/NamedEntityRecognitionUsingNLTKandSpacy/NamedEntityRecognition.py
+++ NLP & DeepLearning/DeepLearning/Keras/InformationExtractionAndNER/NamedEntityRecognitionUsingNLTKandSpacy/NamedEntityRecognition.py
@@ -1,5 +1,12 @@
 # To add a new cell, type '# %%'
 # To add a new markdown cell, type '# %% [markdown]'
+#%%
+# working directory
+import os
+
+cd_path = os.path.dirname(os.path.realpath(__file__))
+os.chdir(cd_path)
+
 # %%
 # Import NLTK
 import nltk
